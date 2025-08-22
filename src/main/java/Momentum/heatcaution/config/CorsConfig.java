6c4937772f4e0f579db61dca51c99357024b2879
DongGyun Kim <78362785+Silverblond@users.noreply.github.com>
2025-08-22 20:53:43 +0900

    Revert "Hotfix"

--- src/main/java/Momentum/heatcaution/config/CorsConfig.java
+++ src/main/java/Momentum/heatcaution/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package Momentum.heatcaution.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,17 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                        .allowedOrigins("https://ondomi.site", "https://www.ondomi.site", "http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                        .allowedHeaders("*") // 모든 헤더 허용
-                        .allowCredentials(true); // 인증 정보(쿠키) 허용
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
+                .allowedOrigins("https://ondomi.site", "https://www.ondomi.site")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 인증 정보(쿠키) 허용
     }
 }
